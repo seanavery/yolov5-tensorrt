b3a6d921fc187bdb8369da2b9663782f36210ee8
sean <seanpollock3344@gmail.com>
2020-07-16 14:55:50 -0500

    simple setup

---
+++ python/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="yolov5-tensorrt-breadofsean",
+    version="0.0.1",
+    author="Sean Pollock",
+    author_email="seanpollock3344@gmail.com",
+    description="yolov5 inference and visualizer",
+    url="https://github.com/SeanAvery/yolov5-tensorrt",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
