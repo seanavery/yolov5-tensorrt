fdf2e4450914e387e2c613d446d373e2d96fbf15
sean <seanpollock3344@gmail.com>
2020-07-17 09:48:49 -0500

    fixing inference

--- python/lib/Processor.py
+++ python/lib/Processor.py
@@ -1,25 +1,27 @@
+import cv2 
+import sys
 import tensorrt as trt
 import pycuda.autoinit
 import pycuda.driver as cuda
-import cv2
 import numpy as np
 
 class Processor():
     def __init__(self):
-        logger = trt.Logger(trt.Logger.INFO)
-        model = 'models/yolov5s-simple-2.trt'
 
-        with open(model, 'rb') as f, trt.Runtime(logger) as runtime:
+        print('setting up Yolov5s-simple.trt processor')
+
+        # load tensorrt engine
+        TRT_LOGGER = trt.Logger(trt.Logger.INFO)
+        TRTbin = 'models/yolov5s-simple-2.trt'
+        with open(TRTbin, 'rb') as f, trt.Runtime(TRT_LOGGER) as runtime:
             engine = runtime.deserialize_cuda_engine(f.read())
-       
         self.context = engine.create_execution_context()
-
+        
         # allocate memory
         inputs, outputs, bindings = [], [], []
         stream = cuda.Stream()
         for binding in engine:
-            size = trt.volume(engine.get_binding_shape(binding)) # * \
-                   # engine.max_batch_size
+            size = trt.volume(engine.get_binding_shape(binding))
             dtype = trt.nptype(engine.get_binding_dtype(binding))
             host_mem = cuda.pagelocked_empty(size, dtype)
             device_mem = cuda.mem_alloc(host_mem.nbytes)
@@ -34,31 +36,45 @@ class Processor():
         self.outputs = outputs
         self.bindings = bindings
         self.stream = stream
-    
+
     def detect(self, img):
-        img = self.pre_process(img)
-        print('pre processed img', img)
-        output = self.inference(img)
+        shape_orig_WH = (img.shape[1], img.shape[0])
+        resized = self.pre_process(img)
+        outputs = self.inference(resized)
+        print('outputs', outputs)
+
+        # img = self.vis.draw_boxes(img, boxes, confs, clss)
+        
         return img
 
     def pre_process(self, img):
-        img = cv2.resize(img, (640, 640)) 
-        img = img.transpose((2, 0, 1))
-        img = np.ascontiguousarray(img)
+        print('original image shape', img.shape)
+        img = cv2.resize(img, (640, 640))
+        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = img.transpose((2, 0, 1)).astype(np.float32)
+        # img /= 255.0
         return img
-    
-    def inference(self, img):
-        self.inputs[0]['host'] = img
 
+    def inference(self, img):
+        # copy img to input memory
+        self.inputs[0]['host'] = np.ascontiguousarray(img)
+
+        # transfer data to the gpu
         for inp in self.inputs:
-            print('inp', inp)
             cuda.memcpy_htod_async(inp['device'], inp['host'], self.stream)
 
+        # run inference
         self.context.execute_async_v2(
-            bindings=self.bindings,
-            stream_handle=self.stream.handle)
+                bindings=self.bindings,
+                stream_handle=self.stream.handle)
 
+        # fetch outputs from gpu
         for out in self.outputs:
-            print('out', out)
             cuda.memcpy_dtoh_async(out['host'], out['device'], self.stream)
+
+
+        # synchronize stream
+        self.stream.synchronize()
+        
+        return [out['host'] for out in self.outputs]
 
