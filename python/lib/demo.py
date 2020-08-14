caead634feea1b18c701f5dafab1a13700385bac
sean <seanpollock3344@gmail.com>
2020-08-14 00:54:00 -0500

    wip visualizations

--- python/lib/demo.py
+++ python/lib/demo.py
@@ -1,6 +1,7 @@
 import cv2
 from Processor import Processor
 from Visualizer import Visualizer
+import sys
 
 def main():
     # setup processor and visualizer
@@ -8,26 +9,28 @@ def main():
     visualizer = Visualizer()
 
     # fetch input
-    img = cv2.imread('./dogs.jpeg')
-     # img = cv2.imread('./sample_720p.jpg')
+    # img = cv2.imread('./dogs.jpeg')
+    img = cv2.imread('./sample_720p.jpg')
 
     # inference
     output = processor.detect(img) 
     img = cv2.resize(img, (640, 640))
 
     # object visualization
-    # object_grids = processor.extract_object_grids(output)
-    # visualizer.draw_object_grid(img, object_grids, 0.1)
+    object_grids = processor.extract_object_grids(output)
+    visualizer.draw_object_grid(img, object_grids, 0.5)
 
-    # # class visualization
-    # class_grids = processor.extract_class_grids(output)
-    # visualizer.draw_class_grid(img, class_grids, 0.01)
+    # class visualization
+    class_grids = processor.extract_class_grids(output)
+    visualizer.draw_class_grid(img, class_grids, 0.01)
 
     # bounding box visualization
     boxes = processor.extract_boxes(output)
     visualizer.draw_boxes(img, boxes)
 
     # final results
+    boxes, confs, classes = processor.post_process(output)
+    visualizer.draw_results(img, boxes, confs, classes)
 
 if __name__ == '__main__':
     main()   
