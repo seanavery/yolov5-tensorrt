c2fb53e14a637749a6e8801cc3abbb17104fe08c
seanavery <seanpollock3344@gmail.com>
2020-11-17 16:54:29 -0500

    more deps

--- python/install.sh
+++ python/install.sh
@@ -1,2 +1,6 @@
 # install pycuda
 sudo pip3 install --global-option=build_ext --global-option="-I/usr/local/cuda/include" --global-option="-L/usr/local/cuda/lib64" pycuda
+# other deps
+pip3 install cython
+sudo apt-get install libjpeg-dev
+pip3 install matplotlib
